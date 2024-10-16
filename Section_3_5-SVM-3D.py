6d6c5ca9c5ad902626d7bb5173ab6935da88a217
robbarto2 <robert_barton@yahoo.com>
2024-10-16 13:52:30 -0700

    Updated Section_3_5-SVM-3D.py

--- Section_3_5-SVM-3D.py
+++ Section_3_5-SVM-3D.py
@@ -12,7 +12,7 @@ svm_rbf = SVC(kernel='rbf', C=1e3, gamma=0.1)
 svm_rbf.fit(X, y)
 
 # Create a mesh to plot decision boundary
-h = .02  # Step size in the mesh
+h = .01  # Step size in the mesh
 x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
 y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
 xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
@@ -23,21 +23,30 @@ Z = svm_rbf.decision_function(np.c_[xx.ravel(), yy.ravel()])
 Z = Z.reshape(xx.shape)
 
 # Plot the original data in 2D
-#plt.figure(figsize=(12, 5))
+plt.figure(figsize=(12, 5))
 
-#plt.subplot(1, 2, 1)
-#plt.scatter(X[:, 0], X[:, 1], c=['red' if label == 1 else 'black' for label in y])
-#plt.title('Original Data in 2D')
-#plt.xlabel('Feature 1')
-#plt.ylabel('Feature 2')
+plt.subplot(1, 2, 1)
+plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Paired, edgecolors='k')
+plt.title('Original Data in 2D')
+plt.xlabel('Feature 1')
+plt.ylabel('Feature 2')
+
+# Plot the decision boundary in 2D
+plt.contourf(xx, yy, Z, levels=np.linspace(Z.min(), 0, 7), cmap=plt.cm.PuBu)
+plt.contour(xx, yy, Z, levels=[0], linewidths=2, colors='darkred')
 
 # Plot the projected data in 3D
 fig = plt.figure(figsize=(10, 6))
 ax = fig.add_subplot(111, projection='3d')
-ax.scatter(X[:, 0], X[:, 1], svm_rbf.decision_function(X), c=y, cmap=plt.cm.Paired)
+ax.scatter(X[:, 0], X[:, 1], svm_rbf.decision_function(X), c=y, cmap=plt.cm.Paired, edgecolors='k')
 
 # Plot decision boundary as a grid
 ax.plot_wireframe(xx, yy, Z, rstride=10, cstride=10, color='gray', alpha=0.3)
+
+# Highlight support vectors
+ax.scatter(svm_rbf.support_vectors_[:, 0], svm_rbf.support_vectors_[:, 1], 
+           svm_rbf.decision_function(svm_rbf.support_vectors_), 
+           s=100, facecolors='none', edgecolors='k', linewidths=1.5)
 
 ax.set_title('Projected Data with Decision Boundary (3D)')
 ax.set_xlabel('Feature 1')
